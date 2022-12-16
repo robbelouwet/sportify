717c16ce0b563e13cd39a22acd49be332a1da3c9
AndresDeJonge <andres.dejonge@student.kdg.be>
2022-12-16 16:56:26 +0100

    WIP

--- src/components/pages/Recommendations.js
+++ src/components/pages/Recommendations.js
@@ -1,5 +1,26 @@
-function component() {
-    return (<div>Recommendations!</div>)
+import {useEffect, useState} from 'react';
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+
+function Recommendation() {
+    const [sports, updateSports] = useState([]);
+
+    initializeApp();
+    const db = getFirestore();
+
+    useEffect(() => {
+        const sport = 'Football'
+        fetch(`http://localhost:4000/recommendations/${sport}`)
+            .then(response => response.json())
+            .then(data => updateSports(data))
+            .catch((error) => {
+                console.error(error)
+            });
+    })
+
+    const sportItems = sports.map(s => <li key={s.sport}>Sport: {s.sport}, Score: {s.score}</li>)
+
+    return (<ul>{sportItems}</ul>)
 }
 
-export default component
+export default Recommendation
