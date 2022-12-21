bb58e5453085a6021e188953f91235ee5846df18
Robbe Louwet <robbe.louwet@the47.be>
2022-12-21 14:45:14 +0100

    added run script

--- back-end/src/app.controller.ts
+++ back-end/src/app.controller.ts
@@ -4,27 +4,36 @@ import { Recommendation } from './recommendation';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+	constructor(private readonly appService: AppService) { }
 
-  @Get('recommendations')
-  getRecommendations(@Query('sports') sports: string): Recommendation[] {
-    console.log(sports)
-    return this.appService.getRecommendations(sports.split(','));
-  }
+	@Get('recommendations')
+	getRecommendations(@Query('sports') sports: string): Recommendation[] {
+		//console.log(sports)
+		return this.appService.getRecommendations(sports.split(','));
+	}
 
-  @Get('most-popular-sports')
-  getMostPopularSports(): string[] {
-    return [
-      'Football',
-      'Hockey',
-      'Tennis',
-      'Volleyball',
-      'Table tennis',
-      'Basketball',
-      'Padel',
-      'Athletics',
-      'Badminton',
-      'Fitness training',
-    ];
-  }
+	@Get("common-tags")
+	getCommonTags(@Query('sports') sports: string): string[] {
+	}
+
+	@Get('most-popular-sports')
+	getMostPopularSports(): string[] {
+		return [
+			'Football',
+			'Hockey',
+			'Tennis',
+			'Volleyball',
+			'Table tennis',
+			'Basketball',
+			'Padel',
+			'Athletics',
+			'Badminton',
+			'Fitness training',
+		];
+	}
+
+	@Get("health-check")
+	getHealthCheck() {
+		return true
+	}
 }
--- back-end/src/app.service.ts
+++ back-end/src/app.service.ts
@@ -5,34 +5,38 @@ import * as ContentBasedRecommender from 'content-based-recommender';
 
 @Injectable()
 export class AppService {
-  private recommender: ContentBasedRecommender;
+	private recommender: ContentBasedRecommender;
 
-  constructor() {
-    this.recommender = new ContentBasedRecommender({
-      minScore: 0.0,
-      maxSimilarDocuments: 100,
-    });
-    this.recommender.train(data);
-  }
+	constructor() {
+		this.recommender = new ContentBasedRecommender({
+			minScore: 0.0,
+			maxSimilarDocuments: 100,
+		});
+		this.recommender.train(data);
+	}
 
-  getRecommendations(sports: string[]): Recommendation[] {
-    let recommendedSports = [];
-    sports.forEach((s) => {
-      recommendedSports = recommendedSports.concat(
-        this.recommender.getSimilarDocuments(s, 0, 10),
-      );
-    });
+	getRecommendations(sports: string[]): Recommendation[] {
+		let recommendedSports = [];
+		sports.forEach((s) => {
+			recommendedSports = recommendedSports.concat(
+				this.recommender.getSimilarDocuments(s, 0, 10),
+			);
+		});
 
-    // 1. Sort on score (DESC)
-    // 2. Remove sports that are preferences of the user
-    // 3. Remove duplicate sports (keep highest score of sport)
-    // 4. Only keep the first 5 recommendations
-    // 5. Map each object to a Recommendation class
-    return recommendedSports
-      .sort((x, y) => y.score - x.score)
-      .filter((v) => !sports.includes(v.id))
-      .filter((v, i, a) => a.findIndex((v2) => v2.id === v.id) === i)
-      .slice(0, 5)
-      .map((doc) => new Recommendation(doc.id, doc.score));
-  }
+		console.log("Doc:", JSON.stringify(recommendedSports))
+
+		// 1. Sort on score (DESC)
+		// 2. Remove sports that are preferences of the user
+		// 3. Remove duplicate sports (keep highest score of sport)
+		// 4. Only keep the first 5 recommendations
+		// 5. Map each object to a Recommendation class
+		return recommendedSports
+			.sort((x, y) => y.score - x.score)
+			.filter((v) => !sports.includes(v.id))
+			.filter((v, i, a) => a.findIndex((v2) => v2.id === v.id) === i)
+			.slice(0, 5)
+			.map((doc) => {
+				return new Recommendation(doc.id, doc.score)
+			});
+	}
 }
