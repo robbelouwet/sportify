f3089a4920650d193b8e30b17a3676f1abb5047d
Robbe Louwet <robbe.louwet@student.kuleuven.be>
2022-12-23 13:01:06 +0100

    added the locations and time of every sport

---
+++ front-end/src/data/sportsEvents.js
@@ -0,0 +1,427 @@
+export default {
+    'Aerobics/Partyrobics': [
+        ['g-gymnastics', 'Gymnasium (GYMN) Grote zaal'],
+        ['Monday 20:00 - 21:00', 'Gymnasium (GYMN) Grote zaal'],
+        ['Wednesday 18:00 - 19:00', 'Gymnasium (GYMN) Grote zaal']
+    ],
+    'Antwerp 10 Miles': [
+        ['Tuesday 19:00 - 20:30', 'Athletics track'],
+        ['Thursday 19:00 - 20:30', 'Athletics track']
+    ],
+    'Archery Tag': [
+        [
+            'Monday 18:00 - 19:00',
+            'Beach- & fitnesshal (BFH) Beachveld 3 Rugbyterrein (R)'
+        ],
+        [
+            'Monday 19:00 - 20:00',
+            'Beach- & fitnesshal (BFH) Beachveld 3 Rugbyterrein (R)'
+        ]
+    ],
+    Athletics: [
+        ['Tuesday 19:00 - 21:00', 'Athletics Track'],
+        ['Wednesday 16:00 - 18:00', 'Athletics Track'],
+        ['Thursday 19:00 - 21:00', 'Athletics Track'],
+        ['Wednesday 18:15 - 20:00', 'Athletics Track']
+    ],
+    Badminton: [
+        [
+            'Monday 19:00 - 20:30',
+            'Gebouw De Nayer (GDN) Grote zaal (B+C)'
+        ],
+        [
+            'Monday 20:30 - 21:45',
+            'Gebouw De Nayer (GDN) Grote zaal (B+C)'
+        ],
+        [
+            'Monday 21:45 - 23:00',
+            'Gebouw De Nayer (GDN) Grote zaal (B+C)'
+        ],
+        [
+            'Thursday 21:00 - 22:00',
+            'Gebouw De Nayer (GDN) Grote zaal (A+B+C)'
+        ],
+        [
+            'Thursday 22:00 - 23:00',
+            'Gebouw De Nayer (GDN) Grote zaal (A+B+C)'
+        ],
+        ['Tuesday 19:00 - 21:00', 'Topsporthal (TSH) 1 & 2']
+    ],
+    Balfolk: [['Tuesday 20:00 - 21:00', 'Gymnasium (GYMN) Grote zaal']],
+    Ballet: [
+        ['Thursday 17:30 - 19:00', 'Gebouw De Nayer (GDN) Spiegelzaal 2'],
+        ['Thursday 19:00 - 19:30', 'Gebouw De Nayer (GDN) Spiegelzaal 2'],
+        ['Thursday 19:30 - 21:00', 'Gebouw De Nayer (GDN) Spiegelzaal 2']
+    ],
+    Baseball: [['Sunday 16:00 - 18:00', 'Gebouw De Nayer - Grote Zaal']],
+    Basketball: [
+        ['Monday 19:00 - 21:00', 'Topsporthal (TSH) Unit 3 & 4'],
+        ['Monday 19:00 - 21:00', 'Topsporthal (TSH) Unit 3 & 4'],
+        ['Wednesday 18:00 - 21:00', 'Topsporthal (TSH) Unit 4']
+    ],
+    'Beach Balance': [
+        [
+            'Tuesday 19:00 - 20:00',
+            'Beach- & fitnesshal (BFH) Beachveld 1'
+        ]
+    ],
+    'Beach Bootcamp': [
+        [
+            'Wednesday 20:00 - 21:00',
+            'Beach- & fitnesshal (BFH) Beachveld 3'
+        ]
+    ],
+    'Beach volleyball': [
+        [
+            'Beginners, Wednesday 18:00 - 19:30',
+            'Beach- & fitnesshal (BFH) - Beachvelden 1 & 2'
+        ],
+        [
+            'Experienced, Wednesday 19:30 - 21:00',
+            'Beach- & fitnesshal (BFH) - Beachvelden 1 & 3'
+        ]
+    ],
+    Bodyshock: [['Wednesday 19:00 - 20:00', 'Sporthal UCLL - Unit 4']],
+    Bodypower: [
+        ['Monday 19:00 - 20:00', 'Gebouw De Nayer (GDN) Spiegelzaal 1'],
+        ['Monday 20:00 - 21:00', 'Gebouw De Nayer (GDN) Spiegelzaal 1'],
+        [
+            'Wednesday 19:00 - 20:00',
+            'Gebouw De Nayer (GDN) Spiegelzaal 1'
+        ],
+        [
+            'Wednesday 20:00 - 21:00',
+            'Gebouw De Nayer (GDN) Spiegelzaal 1'
+        ]
+    ],
+    Bootcamp: [
+        [
+            'Monday 18:00 - 19:00',
+            'indoor atletiekhal (IAH) Throwing fields'
+        ]
+    ],
+    'Boxing Fit': [['Wednesday 18:00 - 19:00', 'Gebouw De Nayer Spiegelzaal 1']],
+    Calisthenics: [
+        ['Wednesday 19:00 - 20:30', 'Gymnasium (GYMN) Gymzaal'],
+        ['Wednesday 20:30 - 22:00', 'Gymnasium (GYMN) Gymzaal']
+    ],
+    Capoeira: [
+        [
+            'Wednesday 21:00 - 23:00',
+            'Gebouw De Nayer (GDN) Spiegelzaal 2'
+        ]
+    ],
+    Cheerleading: [
+        ['Thursday 19:30 - 20:30', 'Gymnasium (GYMN) Grote zaal'],
+        ['Thursday 19:00 - 21:30', 'Gymnasium (GYMN) Grote zaal']
+    ],
+    'Circuit training': [
+        ['Monday 19:00 - 20:00', 'Indoor atletiekhal (IAH)'],
+        ['Monday 20:00 - 21:00', 'Indoor atletiekhal (IAH)'],
+        ['Thursday 18:00 - 19:00', 'Indoor atletiekhal (IAH)']
+    ],
+    Climbing: [
+        ['Monday 18:00 - 22:00', 'Sporthal UCLL - Unit 3'],
+        ['Wednesday 18:00 - 21:00', 'Sporthal UCLL - Unit 4']
+    ],
+    'Couple dance': [
+        ['Tuesday 21:00 - 22:00', 'Gymnasium (GYMN) Grote zaal'],
+        ['Tuesday 22:00 - 23:00', 'Gymnasium (GYMN) Grote zaal']
+    ],
+    Cycling: [
+        [
+            'http://www.facebook.com/groups/272671116185818/',
+            'http://www.facebook.com/groups/272671116185818/'
+        ]
+    ],
+    'Dance around the world': [['Monday 19:00 - 20:00', 'Gebouw De Nayer (GDN) Spiegelzaal 2']],
+    'Dance on the beach': [
+        ['Monday 21:00 - 22:00', 'Beach- & fitnesshal (BFH) Beachveld 3']
+    ],
+    Fatburning: [['Monday 19:00 - 20:00', 'Sporthall UCLL - Unit 5']],
+    Fencing: [
+        [
+            'Wednesday 18:00 - 19:00',
+            'Gebouw De Nayer (GDN) Spiegelzaal 2'
+        ],
+        [
+            'Wednesday 19:00 - 20:00',
+            'Gebouw De Nayer (GDN) Spiegelzaal 2'
+        ],
+        [
+            'Wednesday 20:00 - 21:00',
+            'Gebouw De Nayer (GDN) Spiegelzaal 2'
+        ],
+        ['Wednesday 20:00 - 21:00', 'Gebouw De Nayer Spiegelzaal 2']
+    ],
+    'Figure training': [
+        ['Tuesday 18:00 - 19:00', 'Gebouw De Nayer (GDN) Spiegelzaal 1'],
+        ['Tuesday 19:00 - 20:00', 'Gebouw De Nayer (GDN) Spiegelzaal 1'],
+        [
+            'Wednesday 21:00 - 22:00',
+            'Gebouw De Nayer (GDN) Spiegelzaal 1'
+        ],
+        [
+            'Wednesday 22:00 - 23:00',
+            'Gebouw De Nayer (GDN) Spiegelzaal 1'
+        ]
+    ],
+    Fistball: [['Monday 21:00 - 22:30', 'Sporthal UCLL - U45']],
+    'Fitness training': [
+        ['Tuesday 18:00 - 19:00', 'Indoor Atletiekhal (IAH)'],
+        ['Wednesday 17:30 - 18:30', 'Indoor Atletiekhal (IAH)']
+    ],
+    'Flag football': [['Tuesday 20:00 - 21:30', 'Rugbyterrein (R)']],
+    Football: [['Wednesday 20:00 - 22:00', 'Artificial grass field 1 (KG1)']],
+    Futsal: [
+        ['Tuesday 21:00 - 23:00', 'Topsportshal (TSH) Unit 1,2 & 3'],
+        ['Tuesday 21:00 - 23:00', 'Topsportshal (TSH) Unit 1,2 & 3'],
+        ['Wednesday 21:00 - 23:00', 'Topsportshal (TSH) Unit 1,2 & 3'],
+        ['Thursday 21:00 - 23:00', 'Topsportshal (TSH) Unit 1,2 & 3']
+    ],
+    'Gaelic football': [
+        ['Tuesday 20:00 - 21:30', 'Artificial grass field 2 (KG2)'],
+        ['Friday 18:30 - 20:30', 'Artificial grass field 1 (KG1)']
+    ],
+    Golf: [
+        [
+            'Thursday 19:00 - 20:30',
+            'External location - Winge Golf & Country Club'
+        ]
+    ],
+    Gymnastics: [
+        ['Monday 20:00 - 22:00', 'Gymnasium (GYMN) Gymzaal'],
+        [
+            'Tuesday 20:00 - 22:00',
+            'Gymnasium (GYMN) Gymzaal Grote Zaal Gymnasium'
+        ]
+    ],
+    Handball: [
+        [
+            'Wednesday 20:00 - 21:30',
+            'Gebouw De Nayer (GDN) Grote zaal (A+B+C)'
+        ],
+        [
+            'Wednesday 21:30 - 23:00',
+            'Gebouw De Nayer (GDN) Grote zaal (A+B+C)'
+        ],
+        [
+            'Wednesday 21:30 - 23:00',
+            'Gebouw De Nayer (GDN) Grote zaal (A+B+C)'
+        ]
+    ],
+    'Heels Body Expressive': [['Monday 19:00 - 20:00', 'Gymnasium (GYMN) Grote zaal']],
+    'High intensity training': [
+        ['Thursday 21:00 - 22:00', 'Gebouw De Nayer (GDN) Spiegelzaal 1'],
+        ['Thursday 22:00 - 23:00', 'Gebouw De Nayer (GDN) Spiegelzaal 1']
+    ],
+    'Hip Hop': [['Monday 19:00 - 20:00', 'Campus UCLL/HHH - Spiegelzaal']],
+    'Hip Hop Girly': [['Monday 20:00 - 21:00', 'Campus UCLL/HHH - Spiegelzaal']],
+    Hockey: [
+        ['Tuesday 20:00 - 21:30', 'Artificial grass field 1 (KG1)'],
+        ['Tuesday 20:00 - 21:30', 'Artificial grass field 1 (KG1)'],
+        ['Thursday 20:00 - 21:30', 'Artificial grass field 1 (KG1)']
+    ],
+    'Indoor cycling': [
+        ['Monday 19:00 - 20:00', 'Sporthal UCLL - Unit 6 (floor -1)'],
+        ['Monday 20:00 - 21:00', 'Sporthal UCLL - Unit 6 (floor -1)'],
+        ['Tuesday 20:00 - 21:00', 'Sporthal UCLL - Unit 6 (floor -1)'],
+        ['Wednesday 20:00 - 21:00', 'Sporthal UCLL - Unit 6 (floor -1)'],
+        ['Thursday 20:00 - 21:00', 'Sporthal UCLL - Unit 6 (floor -1)']
+    ],
+    'Indoor football': [
+        ['Tuesday 21:00 - 23:00', 'Topsportshal (TSH) Unit 1,2 & 3'],
+        ['Tuesday 21:00 - 23:00', 'Topsportshal (TSH) Unit 1,2 & 3'],
+        ['Wednesday 21:00 - 23:00', 'Topsportshal (TSH) Unit 1,2 & 3'],
+        ['Thursday 21:00 - 23:00', 'Topsportshal (TSH) Unit 1,2 & 3']
+    ],
+    Judo: [
+        ['Monday 19:30 - 21:00', 'Gebouw De Nayer (GDN) Judozaal'],
+        ['Wednesday 21:00 - 23:00', 'Gebouw De Nayer (GDN) Judozaal'],
+        ['Monday 19:00 - 20:00', 'Gebouw De Nayer (GDN) Judozaal'],
+        ['Wednesday 22:00 - 23:00', 'Gebouw De Nayer (GDN) Judozaal']
+    ],
+    JuJitsu: [
+        ['Monday 18:00 - 19:30', 'Gebouw De Nayer (GDN) Judozaal'],
+        ['Wednesday 18:00 - 19:00', 'Gebouw De Nayer (GDN) Judozaal']
+    ],
+    Karate: [
+        ['Tuesday 19:30 - 21:30', 'Gebouw De Nayer (GDN) Judozaal'],
+        ['Thursday 21:30 - 23:00', 'Gebouw De Nayer (GDN) Judozaal']
+    ],
+    Kayak: [['Wednesday 20:30 - 22:00', 'Swimming pool KU Leuven']],
+    Korfball: [
+        ['Monday 21:00 - 22:00', 'Topsporthal (TSH) Unit 1'],
+        ['Monday 22:00 - 23:00', 'Topsporthal (TSH) Unit 1'],
+        ['Monday 22:00 - 23:00', 'Topsporthal (TSH) Unit 1']
+    ],
+    'Kung Fu': [
+        ['Tuesday 17:30 - 19:30', 'Gebouw De Nayer (GDN) Judozaal'],
+        ['Thursday 18:00 - 20:00', 'Gebouw De Nayer (GDN) Judozaal']
+    ],
+    'Latin Solo Dance': [
+        ['Monday 21:00 - 22:00', 'Gymnasium (GYMN) Grote zaal'],
+        ['Monday 22:00 - 23:00', 'Gymnasium (GYMN) Grote zaal']
+    ],
+    'Lyrical Jazz': [
+        ['Monday 21:00 - 22:00', 'Gebouw De Nayer Spiegelzaal 2'],
+        ['Monday 22:00 - 23:00', 'Gebouw De Nayer Spiegelzaal 2']
+    ],
+    'Modern dance': [
+        ['Tuesday 18:00 - 19:00', 'Gebouw De Nayer Spiegelzaal 2'],
+        ['Tuesday 19:00 - 20:00', 'Gebouw De Nayer Spiegelzaal 2'],
+        ['Tuesday 20:00 - 21:15', 'Gebouw De Nayer Spiegelzaal 2'],
+        ['Tuesday 21:15 - 23:00', 'Gebouw De Nayer Spiegelzaal 2']
+    ],
+    Orienteering: [
+        ['Monday 19:00 - 20:00', 'Gebouw De Nayer'],
+        ['Tuesday 19:00 - 20:30', 'Gebouw De Nayer']
+    ],
+    Padel: [
+        [
+            'Tuesday 18:00 - 19:30',
+            'University Sports Centre - Padel courts'
+        ],
+        [
+            'Tuesday 19:30 - 21:00',
+            'University Sports Centre - Padel courts'
+        ]
+    ],
+    Parasports: [
+        [
+            'Badminton (wheelchair), Thursday 19:30 - 21:00',
+            'Gebouw De Nayer - Grote Zaal'
+        ],
+        [
+            'basketball (wheelchair), Thursday 19:30 - 21:00',
+            'Gebouw De Nayer - Grote Zaal'
+        ],
+        [
+            'Tuesday 16:00 - 18:00, conditiontraining (wheelchair)',
+            'Gebouw De Nayer - Grote Zaal'
+        ],
+        ['Thursday 17:00 - 18:00, g-badminton', 'Gymnasium'],
+        ['Wednesday 15:00 - 16:00, g-gymnastics', 'Gymnasium'],
+        [
+            'Thursday 19:30 - 21:00, g-football',
+            'klein voetbalveld - atletiekpiste'
+        ],
+        [
+            'Monday 13:00 - 16:00, swimming',
+            'Universitair Sportcentrum - zwembad'
+        ],
+        [
+            'Thursday 16:00 - 18:00, swimming',
+            'Universitair Sportcentrum - zwembad'
+        ]
+    ],
+    Parkour: [['Tuesday 18:30 - 20:00', 'Gymnasium (GYMN) Grote zaal']],
+    Partyrobics: [
+        ['Monday 18:00 - 19:00', 'Gymnasium (GYMN) Grote zaal'],
+        ['Monday 20:00 - 21:00', 'Gymnasium (GYMN) Grote zaal'],
+        ['Wednesday 18:00 - 19:00', 'Gymnasium (GYMN) Grote zaal']
+    ],
+    Pilates: [
+        ['Tuesday 21:00 - 22:00', 'Gebouw De Nayer (GDN) Spiegelzaal 1'],
+        ['Tuesday 22:00 - 23:00', 'Gebouw De Nayer (GDN) Spiegelzaal 1'],
+        ['Thursday 20:00 - 21:00', 'Gebouw De Nayer (GDN) Spiegelzaal 1']
+    ],
+    'Rock climbing': [['http://luak.be/', 'http://luak.be/']],
+    'Rope skipping': [['Thursday 18:00 - 19:00', 'Gymnasium (GYMN) Grote zaal']],
+    Roundnet: [
+        [
+            'Wednesday 18:00 - 20:00',
+            'Throwing fields (W) Beach- & Fitnesshal beach field 2'
+        ]
+    ],
+    Rugby: [
+        ['Monday 20:00 - 22:00', 'Rugby field (R)'],
+        ['Wednesday 20:00 - 22:00', 'Rugby field (R)'],
+        ['Thursday 20:00 - 22:00', 'Rugby field (R)']
+    ],
+    Salsa: [['Wednesday 21:00 - 22:00', 'Sporthal UCLL - Unit 5']],
+    'Scuba diving': [
+        ['Monday 21:00 - 22:30', 'Poseidon'],
+        ['Thursday 21:00 - 22:30', 'Poseidon']
+    ],
+    'Self defence': [['Tuesday 21:30 - 23:00', 'Gebouw De Nayer (GDN) Judozaal']],
+    Showdance: [['Monday 20:00 - 21:00', 'Gebouw De Nayer - Spiegelzaal 2']],
+    Slowpitch: [
+        ['Level1, Sunday 12:00 - 14:00', 'Gebouw De Nayer - Grote Zaal'],
+        ['Level 2, Sunday 14:00 - 16:00', 'Gebouw De Nayer - Grote Zaal']
+    ],
+    Snowboarding: [['Monday - departure time is determined weekly', '']],
+    'Social dancing': [
+        ['Tuesday 21:00 - 22:00', 'Gymnasium (GYMN) Grote zaal'],
+        ['Tuesday 22:00 - 23:00', 'Gymnasium (GYMN) Grote zaal']
+    ],
+    Spelunking: [['Wednesday 20:00 - 22:00', 'SPEKUL Leuven']],
+    'Sports degustation': [['Monday 18:00 - 19:00', 'University Sports Centre KU Leuven']],
+    'Step BBB': [
+        ['Monday 21:00 - 22:00', 'Gebouw De Nayer (GDN) Spiegelzaal 1'],
+        ['Monday 22:00 - 23:00', 'Gebouw De Nayer (GDN) Spiegelzaal 1']
+    ],
+    Swimming: [['Tuesday 19:30 - 21:30', 'Domein UCLL/HHH - zwembad']],
+    'Table tennis': [
+        ['Monday 19:00 - 20:00', 'Gebouw De Nayer (GDN) Grote zaal (A)'],
+        ['Monday 20:00 - 21:00', 'Gebouw De Nayer (GDN) Grote zaal (A)'],
+        ['Monday 21:00 - 23:00', 'Gebouw De Nayer (GDN) Grote zaal (A)']
+    ],
+    Taekwondo: [
+        ['Monday 21:00 - 23:00', 'Gebouw De Nayer (GDN) Judozaal'],
+        ['Wednesday 19:00 - 21:00', 'Gebouw De Nayer (GDN) Judozaal']
+    ],
+    'Tai Chi': [['Thursday 20:00 - 21:30', 'Gebouw De Nayer (GDN) Judozaal']],
+    Tennis: [
+        [
+            'Every Friday from 07/10 - 16/12',
+            'Gebouw de Nayer (GDN) Grote zaal (A+B+C)'
+        ],
+        [
+            'Every Friday from 17/02 - 21/04',
+            'Gebouw de Nayer (GDN) Grote zaal (A+B+C)'
+        ],
+        [
+            'Every Tuesday from 25/04 - 13/06',
+            'Tennis artificial gravel (T1-3)'
+        ],
+        [
+            'Every Friday from 28/04 - 16/06',
+            'Tennis artificial gravel (T1-3)'
+        ],
+        [
+            'Every Tuesday 21:00 - 23:00',
+            'Gebouw De Nayer (GDN) Grote zaal (A+B+C)'
+        ]
+    ],
+    Trampoline: [['Wednesday 21:30 - 23:00', 'Gymnasium (GYMN) Grote zaal']],
+    Triathlon: [
+        ['Monday 21:00 - 22:30', 'Swimming pool'],
+        ['Tuesday 18:00 - 20:00', 'Swimming pool'],
+        ['Thursday 18:00 - 20:00', 'Swimming pool']
+    ],
+    'Ultimate Frisbee': [
+        ['Thursday 19:00 - 21:00', 'Artificial grass field 2 (KG2)'],
+        ['Thursday 19:00 - 21:00', 'Artificial grass field 2 (KG2)']
+    ],
+    Volleyball: [
+        ['Monday 21:00 - 23:00', 'Topsporthal (TSH) Units 3 & 4'],
+        ['Wednesday 19:00 - 21:00', 'Topsporthal (TSH) Units 1,2 & 3']
+    ],
+    Waterpolo: [
+        ['Wednesday 18:00 - 19:00', 'Swimming pool (Z)'],
+        ['Wednesday 19:00 - 20:30', 'Swimming pool (Z)'],
+        ['Wednesday 19:00 - 20:30', 'Swimming pool (Z)']
+    ],
+    'X-Fit': [['Wednesday 21:00 - 22:30', 'Indoor Atletiekhal (IAH)']],
+    Yoga: [
+        ['Monday 18:00 - 19:00', 'Gebouw De Nayer (GDN) Spiegelzaal 1'],
+        ['Tuesday 20:00 - 21:00', 'Gebouw De Nayer (GDN) Spiegelzaal 1']
+    ],
+    Zumba: [
+        ['Tuesday 19:00 - 20:00', 'Topsporthal (TSH) Unit 3 & 4'],
+        ['Tuesday 20:00 - 21:00', 'Topsporthal (TSH) Unit 3 & 4'],
+        ['Thursday 19:00 - 20:00', 'Topsporthal (TSH) Unit 3 & 4'],
+        ['Thursday 20:00 - 21:00', 'Topsporthal (TSH) Unit 3 & 4']
+    ]
+}
--- front-end/src/data/test.js
+++ front-end/src/data/test.js
@@ -1,63 +1,42 @@
-const fs = require("fs")
+const fs = require("fs");
+const { parse } = require("csv-parse");
 
-const remove = ["indoor", "outdoor", "large", "beginner", "intermediate", "advance", "specific", "genres", "type", "use", "videos", "online", ""]
+let events = {}
+
+fs.createReadStream("./time_locations.csv")
+    .pipe(parse({ delimiter: ";" }))
+    .on("data", (row) => {
+
+        if (row[0] === '') return
+        if (!Object.keys(events).includes(row[0])) { events[row[0]] = [] }
+
+        const [time, loc] = filter(row[1], row[2])
+        events[row[0]].push([time, loc])
+    })
+    .on("error", (error) => {
+        console.error(error)
+    })
+    .on("end", () => {
+        console.log(events);
+        console.log("finished")
+    })
 
 
-const distinctTags = () => {
-    const _json = JSON.parse(fs.readFileSync("./newSports.json", null, () => null))
-    let allTags = []
-    for (let i = 0; i < _json.length; i++) {
-        allTags = allTags.concat(_json[i].content.split(" "))
+const filter = (time, loc) => {
 
+    let filteredTime = ""
+    for (let i = 0; i < time.length; i++) {
+        if (time.charCodeAt(i) >= 127 || time.charCodeAt(i) < 32) {
+            filteredTime += " "
+        } else filteredTime += time[i]
     }
-    const uniqueSet = new Set(allTags)
-    console.log(uniqueSet)
+
+    let filteredLoc = ""
+    for (let i = 0; i < loc.length; i++) {
+        if (loc.charCodeAt(i) >= 127 || loc.charCodeAt(i) < 32) {
+            filteredLoc += " "
+        } else filteredLoc += loc[i]
+    }
+
+    return [filteredTime, filteredLoc]
 }
-
-const commonTags = (sports) => {
-    const allSports = JSON.parse(fs.readFileSync("./sports.json"))
-    const lowerSports = sports.map(s => s.toLocaleLowerCase())
-    let tags = {}
-
-    allSports.forEach(({ id, content }) => {
-        // For every sport, iterate its tags
-        if (lowerSports.includes(id.toLocaleLowerCase())) {
-
-            // for every tag, count its occurencies
-            content.split(" ").forEach(tag => {
-                if (Object.keys(tags).includes(tag))
-                    tags[tag]++
-                else tags[tag] = 1
-            })
-        }
-    });
-
-    return tags
-}
-
-const allSports = JSON.parse(fs.readFileSync("./sports.json"))
-let contentOnlySPorts = []
-for (let i = 0; i < allSports.length; i++) {
-    const sport = allSports[i]
-    sport.content += sport.tags.join(" ")
-    delete sport.tags
-    contentOnlySPorts.push(sport)
-}
-
-// doe die hiit weg
-contentOnlySPorts = JSON.parse(JSON.stringify(contentOnlySPorts).replace("hiit", "intense"))
-
-//console.log(contentOnlySPorts)
-
-const cTags = commonTags(contentOnlySPorts.map(s => s.id))
-const cTagsOne = Object.entries(cTags).filter(a => a[1] === 1)
-for (tag of cTagsOne) {
-    contentOnlySPorts = JSON.parse(JSON.stringify(contentOnlySPorts).replace(tag[1], " "))
-}
-
-distinctTags()
-
-const fileString = `
-const sports = ${JSON.stringify(contentOnlySPorts)}\nexport default sports
-`
-fs.writeFileSync("./sports.js", fileString)
