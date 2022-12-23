f27fb3884d1bfbe9d4a12ae0866905c9ddc0d29b
Robbe Louwet <robbe.louwet@student.kuleuven.be>
2022-12-23 16:39:12 +0100

    fixed cold start layout

--- front-end/src/components/pages/preferences/Preference.js
+++ front-end/src/components/pages/preferences/Preference.js
@@ -1,16 +1,16 @@
 
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faTrophy} from "@fortawesome/free-solid-svg-icons";
-import {useState} from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrophy } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
-export default function ({sport, isSelected}) {
+export default function ({ sport, isSelected }) {
     return (
         <div className="card">
             <div className="card-body">
                 <h5 className="card-title">
                     <div className="row">
-                        <div className={isSelected ? 'col-2' : 'invisible'}>
-                            <FontAwesomeIcon icon={faTrophy}/>
+                        <div className="col-2" style={{ visibility: isSelected ? '' : 'hidden' }}>
+                            <FontAwesomeIcon icon={faTrophy} />
                         </div>
                         <div className='col-10'>{sport}</div>
                     </div>
